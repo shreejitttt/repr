4f1e0f4bdbf0f0f0880f39368a404b9f5b3940c9
shreejitttt <shreejittshahh@gmail.com>
2025-11-18 07:13:38 +0530

    Update p71.R

--- p71.R
+++ p71.R
@@ -11,8 +11,8 @@ head(Boston)
 
 sum(is.na(Boston))
 summary(Boston)
-
 boxplot(Boston$medv)
+
 Boston <- Boston %>% filter(medv < 50)
 
 cor_matrix <- cor(Boston)
@@ -28,7 +28,7 @@ adjusted_R2 <- summary(multiple_model)$adj.r.squared
 AIC_value <- AIC(multiple_model)
 BIC_value <- BIC(multiple_model)
 
-cat("Adjusted R^2:", adjusted_R2, "\n")
+cat("Adjusted R2:", adjusted_R2, "\n")
 cat("AIC:", AIC_value, "\n")
 cat("BIC:", BIC_value, "\n")
 
@@ -37,19 +37,19 @@ plot(multiple_model, which = 2)
 
 set.seed(123)
 train_control <- trainControl(method = "cv", number = 10)
+
 cv_model <- train(
   medv ~ lstat * rm,
   data = Boston,
   method = "lm",
   trControl = train_control
 )
+
 print(cv_model)
 
 Boston$medv_class <- ifelse(Boston$medv >= 25, 1, 0)
 
-logistic_model <- glm(medv_class ~ lstat * rm,
-                      data = Boston,
-                      family = "binomial")
+logistic_model <- glm(medv_class ~ lstat * rm, data = Boston, family = "binomial")
 summary(logistic_model)
 
 pred_probs <- predict(logistic_model, type = "response")
@@ -57,4 +57,5 @@ roc_curve <- roc(Boston$medv_class, pred_probs)
 
 plot(roc_curve, col = "blue")
 abline(a = 0, b = 1, lty = 2, col = "red")
+
 cat("AUC:", auc(roc_curve), "\n")
